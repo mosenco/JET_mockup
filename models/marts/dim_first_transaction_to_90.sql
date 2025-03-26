50f13d865997a7dc5632657857a5a6f045ca7224
Luca Lo <loluca.owo@gmail.com>
2025-03-26 14:24:43 +0000

    end

--- models/marts/dim_first_transaction_to_90.sql
+++ models/marts/dim_first_transaction_to_90.sql
@@ -6,7 +6,8 @@ with first_transaction_view as (
 )
 
 select count(transaction_id) as first_90_transaction, client_id
-from transaction
+from {{ ref('stg_transaction') }}
 left join first_transaction_view
 using (client_id)
 where DATE_DIFF(transaction_date, first_transaction, DAY) <= 90
+group by client_id
